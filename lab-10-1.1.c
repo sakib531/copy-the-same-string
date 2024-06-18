78928cc9cffad9116c1810ba7c081a61be11e603
sakib531 <142710106+sakib531@users.noreply.github.com>
2024-06-18 10:27:06 +0600

    Add files via upload

---
+++ lab-10-1.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[50]; char s2[50]; int j; int i;
+
+    printf("Enter the value :\n");
+    gets(s2);
+
+    printf("output : %s",strcpy(s1,s2));
+
+
+return 0;
+}
+
